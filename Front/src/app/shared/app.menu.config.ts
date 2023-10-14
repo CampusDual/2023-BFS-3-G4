9b1f2e6167556d03194a87b2660c918c09c960ba
BraisAM <alvarez.martinez.brais@gmail.com>
2023-10-14 15:07:32 +0200

    Menu Destinos con ciudades

--- Front/src/app/shared/app.menu.config.ts
+++ Front/src/app/shared/app.menu.config.ts
@@ -2,7 +2,6 @@ import { MenuRootItem } from 'ontimize-web-ngx';
 
 export const MENU_CONFIG: MenuRootItem[] = [
   { id: 'home', name: 'HOME', icon: 'home', route: '/main/home' },
-  { id: 'towns', name: 'TOWNS', icon: 'apartment', route: '/main/towns' },
-
+  { id: 'destination', name: 'DESTINATION', icon: 'explore', route: '/main/towns' },
   { id: 'logout', name: 'LOGOUT', route: '/login', icon: 'power_settings_new', confirm: 'yes' }
 ];
