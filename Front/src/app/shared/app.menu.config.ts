98bf1025283a9cc7c2497351b9e18757a17eb2c4
BraisAM <alvarez.martinez.brais@gmail.com>
2023-10-24 09:12:59 +0200

    Imagen en Grid y eliminado Logout del menú

--- Front/src/app/shared/app.menu.config.ts
+++ Front/src/app/shared/app.menu.config.ts
@@ -3,5 +3,4 @@ import { MenuRootItem } from 'ontimize-web-ngx';
 export const MENU_CONFIG: MenuRootItem[] = [
   { id: 'home', name: 'HOME', icon: 'home', route: '/main/home' },
   { id: 'destination', name: 'DESTINATION', icon: 'explore', route: '/main/towns' },
-  { id: 'logout', name: 'LOGOUT', route: '/login', icon: 'power_settings_new', confirm: 'yes' }
 ];
