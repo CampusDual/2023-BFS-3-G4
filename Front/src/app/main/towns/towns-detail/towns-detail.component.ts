ca60109dda87f4a567ad74d48a149ffd286baa24
Angel Castineira <angel.r.castineira@gmail.com>
2023-10-25 14:59:27 +0200

    navegacion de ciudad a usuaria-detalle

--- Front/src/app/main/towns/towns-detail/towns-detail.component.ts
+++ Front/src/app/main/towns/towns-detail/towns-detail.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { OFormComponent, OListComponent, OTextInputComponent } from 'ontimize-web-ngx';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-towns-detail',
@@ -12,7 +14,7 @@ export class TownsDetailComponent implements OnInit {
   @ViewChild('townnamefield',{static:true}) townnamefield:OTextInputComponent;
   public custom_name:string; 
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
@@ -27,6 +29,14 @@ export class TownsDetailComponent implements OnInit {
       this.custom_name = event.newValue.value;
     }
   }
+  goToHostDetail(event){
+    const obj = event;
+    type ObjectKey = keyof typeof obj;
+    const myVar = 'id_client' as ObjectKey;
+    var id_client = obj[myVar];
+    console.log(id_client);
+    this.router.navigate(["main/travelers", id_client]);
+  }    
 
 
 }
