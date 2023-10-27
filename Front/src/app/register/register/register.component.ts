a3d415223708c831a881bcb0ba94799717d1bea7
BraisAM <alvarez.martinez.brais@gmail.com>
2023-10-27 11:44:01 +0200

    Registro Acabado

--- Front/src/app/register/register/register.component.ts
+++ Front/src/app/register/register/register.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { ValidatorFn } from '@angular/forms';
+import { Router } from '@angular/router';
+import { NavigationService } from 'ontimize-web-ngx';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +10,60 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  validatorsArray: ValidatorFn[] = []; // Array de validadores personalizados
+  isPasswordModified: boolean = false; // Indicador de si la contraseña ha sido modificada
+  router: Router;
+
+  constructor(
+    router: Router,
+    @Inject(NavigationService) public navigation: NavigationService) {
+
+    this.validatorsArray.push(this.passwordValidator); // Añadir el validador de contraseña al array
+    this.router = router;
+
+  }
+  
 
   ngOnInit() {
   }
+  onPasswordInput() {
+    this.isPasswordModified = true; // La contraseña ha sido modificada
+  }
 
+  passwordValidator(control: any): any {
+    try {
+      const password = control.parent ? control.parent.controls['password'].value : null; // Obtener el valor de la contraseña
+      const passwordConfirm = control.value; // Obtener el valor de la confirmación de contraseña
+
+      if (password !== passwordConfirm) {
+        return { passwordNotMatched: true }; // Las contraseñas no coinciden
+      } else if (!/^(?=.*\d)(?=.*[a-zA-Z])(?=.*[^a-zA-Z\d]).{8,}$/.test(password)) {
+        return { passwordNotSize: true }; // La contraseña no cumple con los requisitos de tamaño
+      } else {
+        return null; // La contraseña es válida
+      }
+    } catch (e) {
+      return null;
+    }
+  }
+
+  passwordMatchValidator(control: any): any {
+
+    try {
+
+      const password = control.parent ? control.parent.controls['password'].value : null; // Obtener el valor de la contraseña
+      const passwordConfirm = control.value; // Obtener el valor de la confirmación de contraseña
+
+      return password === passwordConfirm ? null : { passwordNotMatched: true }; // Las contraseñas no coinciden
+
+    } catch (e) {
+      return null;
+    }
+
+  }
+
+  goToLogin(){
+    this.router.navigate(["/login"]);
+    console.log("Hola toi aqui");
+  }
 }
