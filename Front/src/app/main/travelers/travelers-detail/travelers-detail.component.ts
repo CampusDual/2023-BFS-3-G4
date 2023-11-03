c8284f079a1dc7805cb88c665f7bb6d47e4c34d8
Marcos Véquez Martínez <95914668+MarcosVequez@users.noreply.github.com>
2023-11-03 15:06:50 +0100

    Reservation con Dialog

--- Front/src/app/main/travelers/travelers-detail/travelers-detail.component.ts
+++ Front/src/app/main/travelers/travelers-detail/travelers-detail.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatDialog } from '@angular/material';
 import { Router } from '@angular/router';
 import { OFormComponent } from 'ontimize-web-ngx';
+import { TravelersReservationComponent } from '../travelers-reservation/travelers-reservation.component';
 
 @Component({
   selector: 'app-travelers-detail',
@@ -12,16 +14,31 @@ export class TravelersDetailComponent implements OnInit {
   
   router: Router;
 
-  constructor(router: Router,) { 
+  constructor(
+    router: Router,
+    protected dialog: MatDialog,) { 
     this.router = router;
   }
 
   ngOnInit() {
   }
 
+  public openReservation(data: any): void {
+    let id_client = this.form.getComponents().id_client.getValue();
+    this.dialog.open(TravelersReservationComponent, {
+      
+      height: '330px',
+      width: '520px',
+      data: {
+        id_client_host: id_client
+      },
+    });
+  }
+/*
   reservationFn(){   
     let id_client = this.form.getComponents().id_client.getValue();
     console.log(id_client);
     this.router.navigate(["main/travelers/reservation", id_client]);
   }
+  */
 }
--- Front/src/app/main/travelers/travelers-reservation/travelers-reservation.component.ts
+++ Front/src/app/main/travelers/travelers-reservation/travelers-reservation.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material';
 
 @Component({
   selector: 'app-travelers-reservation',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TravelersReservationComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data: any
+  ) { }
 
   ngOnInit() {
   }
