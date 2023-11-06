1ecdf70f910584f03fb6534806d78f4fafd49fe5
BraisAM <alvarez.martinez.brais@gmail.com>
2023-11-06 11:27:55 +0100

    Chips en grid de anfitrionas y busqueda

--- Front/src/app/main/towns/towns-detail/towns-detail.component.ts
+++ Front/src/app/main/towns/towns-detail/towns-detail.component.ts
@@ -10,54 +10,61 @@ import { Router } from '@angular/router';
 })
 export class TownsDetailComponent implements OnInit {
 
-  @ViewChild('form',{static:true}) form:OFormComponent;
-  @ViewChild('townnamefield',{static:true}) townnamefield:OTextInputComponent;
-  public custom_name:string; 
+  @ViewChild('grid', { static: true }) grid: OGridComponent;
+  @ViewChild('townnamefield', { static: true }) townnamefield: OTextInputComponent;
+  public custom_name: string;
 
-  public arrayActivitiesClient: string[];
+  public arrayActivities: string[];
 
   constructor(private router: Router,
-    private ontimizeServiceUsers: OntimizeService) { 
-      this.ontimizeServiceUsers.configureService(this.ontimizeServiceUsers.getDefaultServiceConfiguration('users'));
-    }
+    private ontimizeServiceUsers: OntimizeService) {
+    this.ontimizeServiceUsers.configureService(this.ontimizeServiceUsers.getDefaultServiceConfiguration('users'));
+  }
 
   ngOnInit() {
   }
 
-  onLoad(){
-    let idclient = this.form.getComponents().id_client.getValue();
-    console.log(idclient);
-    this.ontimizeServiceUsers.query({id_client: idclient}, ['id_activity', 'activity_name'], 'activity_client').subscribe(
-      res => {
-        if (res.data && res.data.length) {
-          this.arrayActivitiesClient = [];
-          res.data.forEach(element => {
-            this.arrayActivitiesClient.push(element.activity_name);
-          });
-        }
-      }      
-    ); 
-    console.log(this.arrayActivitiesClient + "patata") 
-   }
+  splitActivities(activities){
+    if (activities && typeof activities === 'string') {
+      const partes = activities.split(',');
+      return partes;
+    } else {
+      return [];
+    }
+  }
 
-  loadName(){
-   this.custom_name = this.townnamefield.getValue();
+  // loadActivities(idclient) {
+  //   console.log(idclient);
+  //   this.ontimizeServiceUsers.query({ id_client: idclient }, ['id_activity', 'activity_name'], 'activity_client').subscribe(
+  //     res => {
+  //       if (res.data && res.data.length) {
+  //         this.arrayActivitiesClient [idclient] = [];
+  //         res.data.forEach(element => {
+  //           this.arrayActivitiesClient[idclient].push(element.activity_name);
+  //         });
+  //       }
+  //     }
+  //   );
+  // }
+
+  loadName() {
+    this.custom_name = this.townnamefield.getValue();
 
   }
 
-  reloadValues(event){
-    if(event.newValue){
+  reloadValues(event) {
+    if (event.newValue) {
       this.custom_name = event.newValue.value;
     }
   }
-  goToHostDetail(event){
+  goToHostDetail(event) {
     const obj = event;
     type ObjectKey = keyof typeof obj;
     const myVar = 'id_client' as ObjectKey;
     var id_client = obj[myVar];
     console.log(id_client);
     this.router.navigate(["main/travelers", id_client]);
-  }    
+  }
 
- 
+
 }
