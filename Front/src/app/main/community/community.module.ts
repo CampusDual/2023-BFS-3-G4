fd3a6bf2582aaed389ba3b3c90c08c5fbb500393
EstiP13 <esterpino86@gmail.com>
2023-11-07 15:13:12 +0100

    Communities grid
    Primer alquiler "boceto"

--- Front/src/app/main/community/community.module.ts
+++ Front/src/app/main/community/community.module.ts
@@ -1,15 +1,18 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CommunityRoutingModule } from './community-routing.module';
 import { CommunityHomeComponent } from './community-home/community-home.component';
+import { OntimizeWebModule } from 'ontimize-web-ngx';
 
 
 @NgModule({
   declarations: [CommunityHomeComponent],
   imports: [
     CommonModule,
-    CommunityRoutingModule
-  ]
+    CommunityRoutingModule,
+    OntimizeWebModule,
+  ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class CommunityModule { }
