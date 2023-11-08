8e3276f3983f347d90474e00adb14faf89312925
BraisAM <alvarez.martinez.brais@gmail.com>
2023-11-08 10:41:39 +0100

    Navegacion entre comm, towns, host
    Navegacion con click en imagenes en comunidades, ciudades, y anfitrionas

--- Front/src/app/main/community/community-detail/community-detail.component.ts
+++ Front/src/app/main/community/community-detail/community-detail.component.ts
@@ -12,4 +12,18 @@ export class CommunityDetailComponent implements OnInit {
   ngOnInit() {
   }
 
+  getImagePath(townName: string): string {
+    // Formatear nombres de las imagenes de ciudades.
+    const formatName = (name: string) => {
+        // Obviar las tildes y otros caracteres especiales
+        name = name.normalize('NFD').replace(/[\u0300-\u036f]/g, "");
+        // Reemplaza los múltiples espacios con uno solo
+        name = name.replace(/\s+/g, ' ');
+        // Reemplaza los espacios y caracteres no alfanuméricos por guiones bajos
+        return name.toLowerCase().replace(/[\s\W]+/g, '_');
+    };
+
+    return `assets/images/towns_image/${formatName(townName)}.png`;
+  }
+
 }
