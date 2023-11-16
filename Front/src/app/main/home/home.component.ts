e97e1a16016f48adf0966b1752a3bccb4790b523
BraisAM <alvarez.martinez.brais@gmail.com>
2023-11-16 15:04:23 +0100

    Cambio Home para stats
    Añadido grid 5 ciudades con más anfitrionas
    WIP 5 actividades más elegidas

--- Front/src/app/main/home/home.component.ts
+++ Front/src/app/main/home/home.component.ts
@@ -22,4 +22,18 @@ export class HomeComponent implements OnInit {
     this.router.navigate(['../', 'login'], { relativeTo: this.actRoute });
   }
 
+  getImagePath(townName: string): string {
+    // Formatear nombres de las imagenes de ciudades.
+    const formatName = (name: string) => {
+        // Obviar las tildes y otros caracteres especiales
+        name = name.normalize('NFD').replace(/[\u0300-\u036f]/g, "");
+        // Reemplaza los múltiples espacios con uno solo
+        name = name.replace(/\s+/g, ' ');
+        // Reemplaza los espacios y caracteres no alfanuméricos por guiones bajos
+        return name.toLowerCase().replace(/[\s\W]+/g, '_');
+    };
+
+    return `assets/images/towns_image/${formatName(townName)}.png`;
+  }
+
 }
